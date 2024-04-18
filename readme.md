e1556876b9110d462f03cd04a48a8a65853bad3d
George Erimia <l3ttuceftw@gmil.com>
2024-04-18 15:14:31 +0300

    Initial Commit - Added main window

---
+++ readme.md
@@ -0,0 +1,7 @@
+Java Tic Tac Toe Game
+
+This is an attempt to make a tic tac toe game by documenting the Java 2d graphics library
+
+These are links from where I am documenting my project :
+* https://www.geeksforgeeks.org/java-jframe/
+* 
---
+++ src/main/java/org/example/MyFrame.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame() {
+        this.setTitle("Tic Tac Toe");
+        this.setSize(800,800);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.setVisible(true);
+    }
+}
